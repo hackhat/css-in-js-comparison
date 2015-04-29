dde79acc52bd89df9fed0fb5601dc4890025e967
hackhat <hackhat.com@gmail.com>
2015-04-29 21:45:42 +0100

    Adds a comment to absurd.

--- src/client/absurd/index.js
+++ src/client/absurd/index.js
@@ -4,6 +4,7 @@ var Root  = React.createFactory(require('./ui/Root'));
 // Added `module.exports = client();` to the library source, it's faster than adding to webpack config.
 // Easiest way to load is with `var Absurd = require('../absurd');`;
 // Note: would be nice to add the `absurd.compile` only here but it creates a new context each time.
+// In a real project you would use a gulp task to render the CSS file.
 
 
 
