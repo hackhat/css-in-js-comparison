88632493fdb9cbfd2c9de6c63433d3251d1dafef
hackhat <hackhat.com@gmail.com>
2015-05-02 13:46:25 +0100

    Tried to setup react-style with its webpack plugin but it doesn't render the bundle.css as expected.

--- gulpfile.js
+++ gulpfile.js
@@ -8,19 +8,61 @@ var webpackConfig = require('tottys-project').webpackConfig({
 }).settings;
 
 
+
 // Needed for react-style.
+// var ReactStylePlugin = require('react-style-webpack-plugin');
+// var ExtractTextPlugin = require('extract-text-webpack-plugin');
+// webpackConfig.module.loaders.push({
+//     test: /\.js$/,
+//     loader: ReactStylePlugin.loader()
+// })
+// webpackConfig.plugins.push(new ReactStylePlugin('bundle.css'));
 var ReactStylePlugin = require('react-style-webpack-plugin');
-webpackConfig.module.loaders.push({
-    test: /\.js$/,
-    loader: ReactStylePlugin.loader()
-})
-webpackConfig.plugins.push(new ReactStylePlugin('bundle.css'));
-delete webpackConfig.resolve.alias.react
+var ExtractTextPlugin = require('extract-text-webpack-plugin');
+
+webpackConfig = {
+  devtool: 'sourcemap',
+  entry: './src/client/index.js',
+  output: {
+    filename: "bundle.js",
+    path: __dirname + "/dist"
+  },
+  resolve: {
+    alias: {}
+  },
+  custom: {},
+  module: {
+    loaders: [
+      {
+        test: /\.js$/,
+        loaders: [
+          ReactStylePlugin.loader()
+        ]
+      },
+      {
+        test: /\.css$/,
+        loader: ExtractTextPlugin.extract('css-loader')
+      }
+    ]
+  },
+  plugins: [
+    new ReactStylePlugin('bundle.css'),
+    new webpack.DefinePlugin({
+      'process.env': {
+        // To enable production mode:
+        // NODE_ENV: JSON.stringify('production')
+      }
+    })
+  ]
+};
+
+
 
 module.exports = require('tottys-project').gulpfile({
     rootPath      : __dirname,
     webpackConfig : webpackConfig,
 });
+
 
 module.exports = {
     webpackConfig: webpackConfig
--- src/client/react-style/ui/ItemMedia.js
+++ src/client/react-style/ui/ItemMedia.js
@@ -9,15 +9,22 @@ var styles = StyleSheet.create({
     'item-media': {
         background: 'yellow',
         padding : '20px',
-        '@media (min-width: 600px)': {
+    },
+    '@media (min-width: 600px)': {
+        'item-media': {
             background: 'red'
-        },
-        '@media (max-width: 600px)': {
-            background: 'orange'
+        }
+    },
+    '@media (max-width: 600px)': {
+        'item-media': {
+           background: 'orange'
         }
     }
 })
-
+// When you are using the webpack plugin then `styles['item-media']` turns into
+// a string with the class name.
+// If you don't use the webpack plugin then `styles['item-media']` is just an
+// object with the styles.
 
 
 
@@ -32,7 +39,8 @@ var Root = React.createClass({
 
     render: function() {
         return React.DOM.li({
-            className: 'item-media'
+            className: styles['item-media'],
+            // style: styles['item-media']
         },
             React.DOM.p({
             }, this.props.label)
