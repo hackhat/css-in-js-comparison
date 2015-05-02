7d575fa6307e8f3c4a756d73e087ac39cd9c7f38
hackhat <hackhat.com@gmail.com>
2015-05-02 13:51:20 +0100

    Restores gulp and adds comment to react-style.

--- gulpfile.js
+++ gulpfile.js
@@ -9,55 +9,6 @@ var webpackConfig = require('tottys-project').webpackConfig({
 
 
 
-// Needed for react-style.
-// var ReactStylePlugin = require('react-style-webpack-plugin');
-// var ExtractTextPlugin = require('extract-text-webpack-plugin');
-// webpackConfig.module.loaders.push({
-//     test: /\.js$/,
-//     loader: ReactStylePlugin.loader()
-// })
-// webpackConfig.plugins.push(new ReactStylePlugin('bundle.css'));
-var ReactStylePlugin = require('react-style-webpack-plugin');
-var ExtractTextPlugin = require('extract-text-webpack-plugin');
-
-webpackConfig = {
-  devtool: 'sourcemap',
-  entry: './src/client/index.js',
-  output: {
-    filename: "bundle.js",
-    path: __dirname + "/dist"
-  },
-  resolve: {
-    alias: {}
-  },
-  custom: {},
-  module: {
-    loaders: [
-      {
-        test: /\.js$/,
-        loaders: [
-          ReactStylePlugin.loader()
-        ]
-      },
-      {
-        test: /\.css$/,
-        loader: ExtractTextPlugin.extract('css-loader')
-      }
-    ]
-  },
-  plugins: [
-    new ReactStylePlugin('bundle.css'),
-    new webpack.DefinePlugin({
-      'process.env': {
-        // To enable production mode:
-        // NODE_ENV: JSON.stringify('production')
-      }
-    })
-  ]
-};
-
-
-
 module.exports = require('tottys-project').gulpfile({
     rootPath      : __dirname,
     webpackConfig : webpackConfig,
